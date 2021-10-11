89e24318349178f2bf591736a336ab450d379740
Matt Rich <matthew.rich@perficient.com>
2021-10-11 11:43:15 -0600

    Init

---
+++ src/UserDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserDetails () {
+  return (
+    <div style={{marginTop: 20}}>
+      <h1>User Details</h1>
+    </div>
+  );
+}
+
+export default UserDetails;
